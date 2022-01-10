badd3b91d9b839fc7e53a8323904ffe0a7697087
myusername <signups@fabiopacifici.com>
2022-01-11 01:27:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello  <code>🌸🌸🌸🌸</code> World
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
